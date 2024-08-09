9b0244612483b476ae3daf36bf933372975de252
Mohd Saqib <mohdsaqib148183@gmail.com>
2024-08-09 16:17:08 +0530

    add more domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["cdn.builder.io", "b.io", "www.freepik.com", "img.freepik.com"],
     dangerouslyAllowSVG: true,
     remotePatterns: [
       {
@@ -10,6 +11,14 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "b.io",
+      },
+      {
+        protocol: "https",
+        hostname: "www.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
       },
     ],
   },
